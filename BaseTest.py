3b7acd0e9a055c48f2e15de64fbc0eac2464568d
vitaliybizilia <biziliavv@gmail.com>
2016-11-09 15:49:37 +0200

    New test added

--- BaseTest.py
+++ BaseTest.py
@@ -34,7 +34,7 @@ class QuoraTest(unittest.TestCase):
             browserlink = item.find('mylink').text
             #driver.get(browserlink)
             removebuttonXpath = "//a[@class='remove br3']"
-            time.sleep(10)
+            time.sleep(3)
             signinbuttonXpath = "//div[@class='header_signin_with_search_bar action_button']"
             signinlinkXpath = "//a[text() = 'Sign In']"
             loginlinkXpath = "//a[text() = 'I Have a Quora Account']"
